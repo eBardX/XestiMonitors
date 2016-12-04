850d1f9eaa670502e5e67136a210a7a0e61dd4ef
J. G. Pusey <ebardx@gmail.com>
2016-12-04 12:20:08 -0500

    Further doc generation tweaks

--- generate-docs.sh
+++ generate-docs.sh
@@ -5,16 +5,12 @@ set -e
 VERSION=$(sed -En "/s.version[[:space:]]*=/s/^.*\\'(.*)\\'.*$/\1/gp" XestiMonitors.podspec)
 
 if git rev-parse "v$VERSION" >/dev/null 2>&1; then
-    ref="v$VERSION"
+    REF="v$VERSION"
 else
-    ref="$(git rev-parse HEAD)"
+    REF="$(git rev-parse HEAD)"
 fi
 
 jazzy --clean                                                                   \
-      --github_url "https://github.com/eBardX/XestiMonitors"                    \
-      --github-file-prefix "https://github.com/eBardX/XestiMonitors/tree/$ref"  \
-      --module "XestiMonitors"                                                  \
+      --github-file-prefix "https://github.com/eBardX/XestiMonitors/tree/$REF"  \
       --module-version "$VERSION"                                               \
-      --root-url "https://eBardX.github.io/XestiMonitors/reference/"            \
-      --sdk iphoneos                                                            \
       --xcodebuild-arguments "-workspace,Example/XestiMonitors.xcworkspace,-scheme,XestiMonitors"
