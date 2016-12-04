ac0f065ccb5d7b3bbd58af91625d7aacbfd607cb
J. G. Pusey <ebardx@gmail.com>
2016-12-04 12:14:08 -0500

    Simplifying doc generation

--- generate-docs.sh
+++ generate-docs.sh
@@ -1,27 +1,20 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 set -e
 
-gituser="eBardX"
-module="XestiMonitors"
-podspec="$module.podspec"
-scheme="$module"
-version=$(sed -En "/s.version[[:space:]]*=/s/^.*\\'(.*)\\'.*$/\1/gp" $podspec)
-workspace="Example/$module.xcworkspace"
+VERSION=$(sed -En "/s.version[[:space:]]*=/s/^.*\\'(.*)\\'.*$/\1/gp" XestiMonitors.podspec)
 
-if git rev-parse "v$version" >/dev/null 2>&1; then
-    # Use the tagged commit when we have one
-    ref="v$version"
+if git rev-parse "v$VERSION" >/dev/null 2>&1; then
+    ref="v$VERSION"
 else
-    # Otherwise, use the current commit.
     ref="$(git rev-parse HEAD)"
 fi
 
-jazzy --clean                                                               \
-      --github_url "https://github.com/$gituser/$module"                    \
-      --github-file-prefix "https://github.com/$gituser/$module/tree/$ref"  \
-      --module "$module"                                                    \
-      --module-version "$version"                                           \
-      --root-url "https://$gituser.github.io/$module/reference/"            \
-      --sdk iphoneos                                                        \
-      --xcodebuild-arguments "-workspace,$workspace,-scheme,$scheme"
+jazzy --clean                                                                   \
+      --github_url "https://github.com/eBardX/XestiMonitors"                    \
+      --github-file-prefix "https://github.com/eBardX/XestiMonitors/tree/$ref"  \
+      --module "XestiMonitors"                                                  \
+      --module-version "$VERSION"                                               \
+      --root-url "https://eBardX.github.io/XestiMonitors/reference/"            \
+      --sdk iphoneos                                                            \
+      --xcodebuild-arguments "-workspace,Example/XestiMonitors.xcworkspace,-scheme,XestiMonitors"
