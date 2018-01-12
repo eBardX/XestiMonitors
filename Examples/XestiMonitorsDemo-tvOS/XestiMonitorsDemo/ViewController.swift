9293075ec224bf2e1e82ec9cf5408618ccf3e548
J. G. Pusey <ebardx@gmail.com>
2018-01-11 19:15:59 -0500

    Added skeletal app and pods

---
+++ Examples/XestiMonitorsDemo-tvOS/XestiMonitorsDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XestiMonitorsDemo
+//
+//  Created by John Pusey on 1/11/18.
+//  Copyright © 2018 Xesticode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
