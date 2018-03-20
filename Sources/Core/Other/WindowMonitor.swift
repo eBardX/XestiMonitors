cf6468562e768229a04869e6abeb3cd78bf34365
mungaiandela <martin.mungai@andela.com>
2018-03-20 12:43:37 +0300

    First attempt to monitor window events

--- Sources/Core/Other/WindowMonitor.swift
+++ Sources/Core/Other/WindowMonitor.swift
@@ -16,6 +16,17 @@
     ///
     
     public class WindowMonitor: BaseNotificationMonitor {
+        ///
+        /// Encapsulates changes to the visibility of the window and to the frame
+        /// of the window.
+        ///
+        public enum Event {
+            
+            case didBecomeVisible(UIWindow)
+            case didBecomeHidden(UIWindow)
+            case didBecomeKey(UIWindow)
+            case didResignKey(UIWindow)
+        }
         
         public struct Options: OptionSet {
             
@@ -53,6 +64,64 @@
             }
             
             public let rawValue: UInt
+        }
+        
+        ///
+        /// Initializes a new `WindowMonitor`.
+        ///
+        /// - Parameters:
+        ///   - queue:      The operation queue on which the handler executes. By
+        ///                 default, the main operation queue is used.
+        ///   - options:    The options that specify which events to monitor. By
+        ///                 default, all events are monitored.
+        ///   - handler:    The handler to call when the visibility of the keyboard
+        ///                 or the frame of the keyboard changes or is about to
+        ///                 change.
+        ///
+        
+        public init(queue: OperationQueue = .main,
+                    window: UIWindow,
+                    options: Options = .all,
+                    handler: @escaping (Event) -> Void) {
+            self.handler = handler
+            self.options = options
+            self.window = window
+            
+            super.init(queue: queue)
+        }
+        
+        public let handler: (Event) -> Void
+        public let options: Options
+        public let window: UIWindow
+        
+        public override func addNotificationObservers() {
+            super.addNotificationObservers()
+            
+            if options.contains(.didBecomeHidden) {
+                observe(.UIWindowDidBecomeHidden, object: window) { _ in
+                    self.handler(.didBecomeHidden(self.window))
+                }
+            }
+            
+            if options.contains(.didBecomeKey) {
+                observe(.UIWindowDidBecomeKey, object: window) { _ in
+                    self.handler(.didBecomeKey(self.window))
+                }
+            }
+            
+            if options.contains(.didBecomeVisible) {
+                observe(.UIWindowDidBecomeVisible, object: window) { _ in
+                    self.handler(.didBecomeVisible(self.window))
+                }
+            }
+            
+            if options.contains(.didResignKey) {
+                observe(.UIWindowDidResignKey, object: window) { _ in
+                    self.handler(.didResignKey(self.window))
+                }
+            }
+            
+           
         }
     }
 
