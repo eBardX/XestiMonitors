e12a9e2b4958ca0ac9e0cc90c83a2c40b00b9b50
Descartess <nyondopaul23@gmail.com>
2018-04-06 10:14:59 +0300

    Add Screen connection monitor

---
+++ Tests/Screen/ScreenConnectionMonitorTests.swift
@@ -0,0 +1,78 @@
+//
+//  ScreenConnectionMonitorTests.swift
+//  XestiMonitors
+//
+//  Created by Paul Nyondo on 2018-04-04.
+//
+//  © 2018 J. G. Pusey (see LICENSE.md)
+//
+
+import UIKit
+import XCTest
+@testable import XestiMonitors
+
+internal class ScreenConnectionMonitorTest: XCTestCase {
+    let notificationCenter = MockNotificationCenter()
+    let screen = MockScreen()
+
+    override func setUp() {
+        super.setUp()
+        NotificationCenterInjector.inject = { return self.notificationCenter }
+    }
+
+    func testMonitor_didConnect() {
+        let expectation = self.expectation(description: "Handler called")
+        var expectedEvent: ScreenConnectionMonitor.Event?
+        let monitor = ScreenConnectionMonitor(screen: screen,
+                                              options: .didConnect,
+                                              queue: .main) { event in
+                                                expectedEvent = event
+                                                expectation.fulfill()
+        }
+
+        monitor.startMonitoring()
+        simulateDidConnect()
+        waitForExpectations(timeout: 1)
+        monitor.stopMonitoring()
+
+        if let event = expectedEvent,
+            case let .didConnect(test) = event {
+            XCTAssertEqual(test, screen)
+        } else {
+            XCTFail("Unexpected event")
+        }
+    }
+
+    func testMonitor_didDisconnect() {
+        let expectation = self.expectation(description: "Handler called")
+        var expectedEvent: ScreenConnectionMonitor.Event?
+        let monitor = ScreenConnectionMonitor(screen: screen,
+                                              options: .didDisconnect,
+                                              queue: .main) { event in
+                                                expectedEvent = event
+                                                expectation.fulfill()
+        }
+
+        monitor.startMonitoring()
+        simulateDidDisconnect()
+        waitForExpectations(timeout: 1)
+        monitor.stopMonitoring()
+
+        if let event = expectedEvent,
+            case let .didDisconnect(test) = event {
+            XCTAssertEqual(test, screen)
+        } else {
+            XCTFail("Unexpected event")
+        }
+    }
+
+    private func simulateDidConnect() {
+        notificationCenter.post(name: .UIScreenDidConnect,
+                                object: screen)
+    }
+
+    private func simulateDidDisconnect() {
+        notificationCenter.post(name: .UIScreenDidDisconnect,
+                                object: screen)
+    }
+}
