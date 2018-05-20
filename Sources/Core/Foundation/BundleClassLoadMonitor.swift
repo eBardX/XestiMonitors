99b87bafa4e8a305f72ac4556886e8214008094f
J. G. Pusey <ebardx@gmail.com>
2018-05-20 10:11:24 -0400

    Partial implementation

---
+++ Sources/Core/Foundation/BundleClassLoadMonitor.swift
@@ -0,0 +1,61 @@
+//
+//  BundleClassLoadMonitor.swift
+//  XestiMonitors
+//
+//  Created by J. G. Pusey on 2018-05-20.
+//
+//  © 2018 J. G. Pusey (see LICENSE.md)
+//
+
+import Foundation
+
+///
+/// A `BundleClassLoadMonitor` instance monitors a bundle for changes to ...
+///
+public class BundleClassLoadMonitor: BaseNotificationMonitor {
+    ///
+    /// Encapsulates changes to ...
+    ///
+    public enum Event {
+        ///
+        ///
+        ///
+        case didLoad(Bundle, [String])
+    }
+
+    ///
+    /// Initializes a new `BundleClassLoadMonitor`.
+    ///
+    /// - Parameters:
+    ///   - bundle:     The bundle to monitor.
+    ///   - queue:      The operation queue on which the handler executes.
+    ///                 By default, the main operation queue is used.
+    ///   - handler:    The handler to call when ...
+    ///
+    public init(bundle: Bundle,
+                queue: OperationQueue = .main,
+                handler: @escaping (Event) -> Void) {
+        self.bundle = bundle
+        self.handler = handler
+
+        super.init(queue: queue)
+    }
+
+    ///
+    /// The bundle being monitored.
+    ///
+    public let bundle: Bundle
+
+    private let handler: (Event) -> Void
+
+    override public func addNotificationObservers() {
+        super.addNotificationObservers()
+
+        observe(Bundle.didLoadNotification,
+                object: bundle) { [unowned self] in
+                    if let bundle = $0.object as? Bundle {
+                        self.handler(.didLoad(bundle, []))
+                    }
+        }
+    }
+}
