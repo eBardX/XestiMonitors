508b2094b7a6596b0ce48a05be6698f72a92c840
Paul Nyondo <paul.musisi@andela.com>
2018-06-09 05:06:04 +0300

    Added current locale monitor (#75)

---
+++ Tests/Foundation/CurrentLocaleMonitorTests.swift
@@ -0,0 +1,47 @@
+//
+//  CurrentLocaleMonitorTests.swift
+//  XestiMonitorsTests
+//
+//  Created by Paul Nyondo on 2018-05-28.
+//
+//  © 2018 J. G. Pusey (see LICENSE.md)
+//
+
+import Foundation
+import XCTest
+@testable import XestiMonitors
+
+internal class CurrentLocaleMonitorTests: XCTestCase {
+    let notificationCenter = MockNotificationCenter()
+
+    override func setUp() {
+        super.setUp()
+        NotificationCenterInjector.inject = {
+            return self.notificationCenter
+        }
+    }
+
+    func testMonitor_CurrentLocaleDidChange() {
+        let expectation = self.expectation(description: "Handler called")
+        var expectedEvent: CurrentLocaleMonitor.Event?
+        let monitor = CurrentLocaleMonitor(queue: .main) { event in
+            expectedEvent = event
+            expectation.fulfill()
+        }
+
+        monitor.startMonitoring()
+        simulateCurrentLocaleDidChange()
+        waitForExpectations(timeout: 1)
+        monitor.stopMonitoring()
+
+        if let event = expectedEvent {
+            XCTAssertEqual(.didChange, event)
+        } else {
+            XCTFail("Unexpected event")
+        }
+    }
+
+    private func simulateCurrentLocaleDidChange() {
+        notificationCenter.post(name: NSLocale.currentLocaleDidChangeNotification, object: nil)
+    }
+}
